9f401e37152277d41ffdf7288b9f058ec8786fa9
hyunsu15 <wjdgustn15@naver.com>
2025-10-29 23:11:38 +0900

    fix : Error 태그가 두번 나오는 버그 수정

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,7 +2,6 @@ package lotto.domain;
 
 import java.util.Collections;
 import java.util.List;
-import lotto.util.ErrorMessageMaker;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -16,13 +15,13 @@ public class Lotto {
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException(ErrorMessageMaker.getErrorMessage("로또 번호는 6개여야 합니다."));
+            throw new IllegalArgumentException("로또 번호는 6개여야 합니다.");
         }
         if (numbers.stream().anyMatch(n -> MIN_NUMBER > n || n > MAX_NUMBER)) {
-            throw new IllegalArgumentException(ErrorMessageMaker.getErrorMessage("로또 번호는 1-45사이여야 합니다."));
+            throw new IllegalArgumentException("로또 번호는 1-45사이여야 합니다.");
         }
         if (numbers.stream().distinct().count() != numbers.size()) {
-            throw new IllegalArgumentException(ErrorMessageMaker.getErrorMessage("로또 번호는 중복이 있으면 안됩니다."));
+            throw new IllegalArgumentException("로또 번호는 중복이 있으면 안됩니다.");
         }
     }
 
