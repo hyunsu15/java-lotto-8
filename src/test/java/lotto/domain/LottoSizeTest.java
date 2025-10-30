3d1f8c25baf70e1fa313691d8de272264e7ffe99
hyunsu15 <wjdgustn15@naver.com>
2025-10-30 09:40:47 +0900

    test: 구현체 테스트보다 상위 테스트
    구현체는 바뀔수있는데, 구현테스트함

--- src/test/java/lotto/domain/LottoSizeTest.java
+++ src/test/java/lotto/domain/LottoSizeTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-class LottoSizeValidatorTest {
-
+class LottoSizeTest {
     @ParameterizedTest
     @CsvSource({
             "1",
@@ -16,7 +15,7 @@ class LottoSizeValidatorTest {
     })
     @DisplayName("값이 천단위가 아니면, 예외를 반환한다.")
     void test1(int value) {
-        Assertions.assertThatThrownBy(() -> LottoSizeValidator.validate(value));
+        Assertions.assertThatThrownBy(() -> new LottoSize(value));
     }
 
     @ParameterizedTest
@@ -27,7 +26,7 @@ class LottoSizeValidatorTest {
     })
     @DisplayName("값이 양수가 아니면, 예외를 반환한다.")
     void test2(int value) {
-        Assertions.assertThatThrownBy(() -> LottoSizeValidator.validate(value));
+        Assertions.assertThatThrownBy(() -> new LottoSize(value));
     }
 
     @ParameterizedTest
@@ -37,6 +36,6 @@ class LottoSizeValidatorTest {
     })
     @DisplayName("나머지는 예외가 나오지 않는다.")
     void test3(int value) {
-        Assertions.assertThatCode(() -> LottoSizeValidator.validate(value)).doesNotThrowAnyException();
+        Assertions.assertThatCode(() -> new LottoSize(value)).doesNotThrowAnyException();
     }
 }
